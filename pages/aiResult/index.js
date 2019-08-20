3ba001d4d526bd891f1aaf939aeb7deff3e58ded
Olivia529 <miaoyue529@163.com>
2019-08-20 22:31:41 +0800

    add buttons

--- pages/aiResult/index.js
+++ pages/aiResult/index.js
@@ -5,15 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    aiResult:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({ aiResult: "病毒性感冒哈哈哈哈哈哈哈哈哈哈"
-    + "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈" })
+    this.setData({ aiResult: "asdfas" })
+    
   },
 
   /**
